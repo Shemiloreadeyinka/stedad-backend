7fc829458537f9b6c869718ce76be7afd15e69c1
shemiloreadeyinka <adeyinkasemilore@gmail.com>
2026-03-30 17:03:53 +0100

    new cors fix

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,6 @@ app.use(cors({
      methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true
   }));
-app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
